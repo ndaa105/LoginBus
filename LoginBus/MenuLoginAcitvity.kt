8b612c020c3683d254d77e3d817480f44ca14ca7
Vinawanda Khodijah <vinawanda518@gmail.com>
2024-07-09 08:50:36 +0700

    andro1

---
+++ LoginBus/MenuLoginAcitvity.kt
@@ -0,0 +1,12 @@
+package com.example.busapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_login)
+    }
+}
